6be2f20fee028e2fa1a918a74fa1b4dc09087ad7
Xen0mrefne <x3n0g3n1c@gmail.com>
2023-03-07 19:08:53 -0300

    HTML & CSS Completed from sketch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
 import { NavbarComponent } from './components/header/navbar/navbar.component';
-=======
+import { MainComponent } from './components/main/main.component';
 import { BannerComponent } from './components/header/banner/banner.component';
->>>>>>> banner
+import { AboutComponent } from './components/main/about/about.component';
+import { EducationComponent } from './components/main/education/education.component';
+import { SkillsComponent } from './components/main/skills/skills.component';
+import { ProjectsComponent } from './components/main/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    NavbarComponent
-=======
-    BannerComponent
->>>>>>> banner
+    NavbarComponent,
+    MainComponent,
+    BannerComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
